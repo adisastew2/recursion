56cc80aab3c78c61a36bfecac6a4db3690ca8ab2
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-26 19:23:38 -0700

    solve getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var matches = [];
+  var treeClimb = function (node) {
+    if (node.className.includes(className)) {
+      matches.push(node);
+    }
+    _.each(node.children, (child)=>{
+      treeClimb(child);
+    });
+  };
+  treeClimb(document.body);
+  return matches;
 };
