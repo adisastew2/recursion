816f59fbbdca073fc70a22c721324d002d322a74
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-27 10:03:39 -0700

    solve stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj + '';
+  } else if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return 'undefined';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var strArr = [];
+    _.each(obj, (item)=>{
+      if (typeof item !== 'function' && item !== undefined) {
+        strArr.push(stringifyJSON(item));
+      }
+      
+    });
+    return '[' + strArr.join(',') + ']';
+  } else {
+    var strArr = [];
+    _.each(obj, (item, i)=>{
+      if (typeof item !== 'function' && item !== undefined) {
+        strArr.push(stringifyJSON(i) + ':' + stringifyJSON(item));
+
+      }
+    });
+    return '{' + strArr.join(',') + '}';
+  }
+  
 };
